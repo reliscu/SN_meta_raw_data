8c89225128a731e9eb38d33afa48bbc37a06fbee
reliscu <rebeliscu@gmail.com>
2023-04-26 15:00:44 -0700

    schirmer

--- schirmer_2019/log.sh
+++ schirmer_2019/log.sh
@@ -4,18 +4,23 @@
 cd /mnt/bdata/rebecca/SCSN_meta_analysis/datasets/schirmer_2019/raw_data
 
 for ea in $(cat ../SRR_Acc_List.txt); do 
-  prefetch $ea --output-file ${ea}.sra
+  prefetch $ea --max-size 200GB --output-file ${ea}.sra
   fasterq-dump -b 100GB -c 100GB -m 100GB -S -p ${ea}.sra
 done
 
+for ea in *fastq; do pigz -p 6 $ea; done
+
 ## Edit filenames to be compatible with Cellranger:
 
-for ea in *_1.fastq; do mv $ea $(echo $ea | sed s/_1/_S1_L001_R1_001/); done
-for ea in *_2.fastq; do mv $ea $(echo $ea | sed s/_2/_S1_L001_R2_001/); done
+for ea in *_1.fastq.gz; do mv $ea $(echo $ea | sed s/_1/_S1_L001_R1_001/); done
+for ea in *_2.fastq.gz; do mv $ea $(echo $ea | sed s/_2/_S1_L001_R2_001/); done
 
-## Make folder per sample and gzip:
+## Make folder per sample
 
-nice -n -18 pigz -p 6 ${geo}/*
+for ea in $(cat ../SRR_Acc_List.txt); do 
+  mkdir $ea
+  mv ${ea}*.fastq.gz $ea
+done
 
 ## Align reads:
 ## Note: Cellranger v7+ counts intronic reads by default
@@ -27,13 +32,13 @@ cellranger="/opt/cellranger-7.1.0/bin/cellranger"
 
 ref="/home/shared/hg_align_db/GRCh38_Cellranger_gencode_v32_ensembl_98/refdata-gex-GRCh38-2020-A"
 
-samples=$(awk -F',' 'NR>1{print substr($1,2,length($1)-2)}' ../sampleinfo.csv | uniq)
+samples=$(awk -F',' 'NR>1{print substr($2,2,length($2)-2)}' ../sampleinfo.csv | uniq)
 
 for ea in $samples; do
-  nice -n -18 $cellranger count \
+  $cellranger count \
     --id=$ea \
     --fastqs=../raw_data/${ea} \
     --sample=$ea \
     --transcriptome=$ref \
-    --localcores=8
+    --localcores=10
 done
